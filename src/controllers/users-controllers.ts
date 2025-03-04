7d634444e72aa3a1694c2e134d80ed5a6f384c28
pedrohfc <pedrofreitas_ph@hotmail.com>
2025-03-03 23:09:57 -0300

    user validation zod

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,8 +1,24 @@
 
 import { Request, Response } from "express";
+import z from "zod";
 
 class UserController {
     create(request: Request, response: Response) {
+
+        // const requiredEmail = user.required({
+        //   email: true,
+        // });
+
+        const bodySchema = z.object({
+            name: z.string().trim().min(2),
+            email: z.string().email(),
+            password: z.string().min(5)
+        })
+
+        // vai no insomia e pega as info dada no json  
+        const {name, email, password} = bodySchema.parse(request.body)
+
+
         return response.json({message: "ok"})
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import { app } from "@/app";
+import "dotenv/config";
+
 
 const PORT = 3333;
+
+console.log("DB_link:", process.env.DATABASE_URL);
 
 app.listen(PORT, () => console.log(`Serer is running on port ${PORT}`));
