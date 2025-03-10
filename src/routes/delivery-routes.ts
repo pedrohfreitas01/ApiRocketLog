9298aefab50a96a8d40fb299ca3646c42a31acf9
pedrohfc <pedrofreitas_ph@hotmail.com>
2025-03-10 10:13:26 -0300

    midlewaes Auth Autor

--- src/routes/delivery-routes.ts
+++ src/routes/delivery-routes.ts
@@ -1,12 +1,17 @@
 import { DeliveriesController } from "@/controllers/delivery-controller";
 import { ensureAuthenticated } from "@/middlewares/ensure-authcated";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthor";
 import { Router } from "express";
 
 const deliveriesRoutes = Router()
 const deliveriesController = new DeliveriesController()
 
-deliveriesRoutes.use(ensureAuthenticated)
+deliveriesRoutes.use(
+  ensureAuthenticated,
+  verifyUserAuthorization(["sale"])
+);
 deliveriesRoutes.post("/", deliveriesController.create)
 
 
 export { deliveriesRoutes };
+
