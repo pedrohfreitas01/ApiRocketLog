d75f4a2bf05f013eb7dcd0b32384264a8cd4a28b
pedrohfc <pedrofreitas_ph@hotmail.com>
2025-03-11 20:50:52 -0300

    status-delivery controller

--- src/controllers/delivery-controller.ts
+++ src/controllers/delivery-controller.ts
@@ -1,8 +1,34 @@
+import { prisma } from "@/database/prisma";
 import { Request, Response } from "express";
+import z from "zod";
 
 class DeliveriesController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "ok" });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      user_id: z.string().uuid(),
+      description: z.string(),
+    });
+
+    const { user_id, description } = bodySchema.parse(request.body);
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description,
+      },
+    });
+
+    return response.status(201).json();
+  }
+
+  async index(request: Request, response: Response) {
+    const deliveries = await prisma.delivery.findMany({
+      include: {
+        user: { select: { name: true, email: true } },
+      },
+    });
+
+    return response.json(deliveries);
   }
 }
 
--- src/routes/delivery-routes.ts
+++ src/routes/delivery-routes.ts
@@ -1,17 +1,17 @@
 import { DeliveriesController } from "@/controllers/delivery-controller";
+import { StatusDeliveryController } from "@/controllers/status-delivery-controller";
 import { ensureAuthenticated } from "@/middlewares/ensure-authcated";
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthor";
 import { Router } from "express";
 
-const deliveriesRoutes = Router()
-const deliveriesController = new DeliveriesController()
+const deliveriesRoutes = Router();
+const deliveriesController = new DeliveriesController();
+const statusDeliveryController = new StatusDeliveryController()
 
-deliveriesRoutes.use(
-  ensureAuthenticated,
-  verifyUserAuthorization(["sale"])
-);
-deliveriesRoutes.post("/", deliveriesController.create)
+deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
+deliveriesRoutes.post("/", deliveriesController.create);
+deliveriesRoutes.get("/", deliveriesController.index);
 
+deliveriesRoutes.patch("/:id/status", statusDeliveryController.update);
 
 export { deliveriesRoutes };
-
